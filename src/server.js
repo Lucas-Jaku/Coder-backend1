613de69bc7408b261a0a0a4039bc31b61d0f6b56
Lucas Jaku <lucasjaku2000@gmail.com>
2026-03-31 19:08:34 -0300

    pre-entregafinal

--- src/server.js
+++ src/server.js
@@ -1,65 +1,19 @@
 import express from "express";
-import handlebars from "express-handlebars";
-import viewsRouter from "./routes/views.router.js";
-import { Server } from "socket.io";
-import path from "path";
-
-// Importación de Managers
-import { productManager } from "./managers/ProductManager.js"; 
-import { msgManager } from "./managers/Messages.manager.js"; 
+import { connectMongoDB } from "./config/db-connection.js";
+import productRouter from './routes/product-router.js'
 
 const app = express();
-const PORT = 8080;
 
-// Middlewares iniciales
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(process.cwd(), "src", "public")));
 
-// Configuración de Handlebars
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(process.cwd(), "src", "views"));
+app.use('/products', productRouter)
 
-// Middleware para inyectar Socket.io en las peticiones (útil para controladores externos)
-app.use((req, res, next) => {
-    req.io = socketServer;
-    next();
-});
+const PORT = process.env.PORT || 3000;
 
-// Rutas
-app.use('/', viewsRouter); 
+connectMongoDB()
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((error) => console.log(error));
 
-const httpServer = app.listen(PORT, () => {
-    console.log(`🚀 Server listening on port ${PORT} - http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server ok on port ${PORT}`));
 
-// Configuración de Socket.io
-const socketServer = new Server(httpServer);
-
-socketServer.on('connection', async (socket) => {
-    console.log(`Cliente conectado: ${socket.id}`);
-
-    // 1. Carga inicial: Enviamos productos y mensajes guardados en src/db/
-    socket.emit('updateProducts', await productManager.getProducts());
-    socket.emit('messages', await msgManager.getAll());
-
-    // 2. Lógica de Productos (WebSockets)
-    socket.on('addProduct', async (newProd) => {
-        await productManager.addProduct(newProd);
-        // Emitimos a TODOS los clientes conectados la lista actualizada
-        socketServer.emit('updateProducts', await productManager.getProducts());
-    });
-
-    socket.on('deleteProduct', async (id) => {
-        await productManager.deleteProduct(id);
-        socketServer.emit('updateProducts', await productManager.getProducts());
-    });
-
-    // 3. Lógica de Chat (WebSockets)
-    socket.on('message', async (msg) => {
-        // msg debe ser { user: "nombre", message: "texto" }
-        await msgManager.create(msg);
-        socketServer.emit('messages', await msgManager.getAll());
-    });
-});
